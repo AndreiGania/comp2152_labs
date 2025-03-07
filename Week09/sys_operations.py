52fd5102fc2b0936064cf94f82ae0c3ca46ee5be
Iznmav <andreijhosephgania@gmail.com>
2025-03-07 15:33:01 -0500

    sys_operations update

--- Week09/sys_operations.py
+++ Week09/sys_operations.py
@@ -1,8 +1,15 @@
 import platform
 import socket
+import os
+import sys
+
 print(f"\n Machine Type: {platform.machine()}")
 
 print(f"\n Processor Type: {platform.architecture()}")
 
 socket.setdefaulttimeout(50)
 print(f"\n Default Timeout for Socket: {socket.getdefaulttimeout()}")
+
+print(f"\n OS Type: {os.name}")
+print(f"\n OS Name: {platform.system()}")
+print(f"\n Current PID: {os.getpid()}")
