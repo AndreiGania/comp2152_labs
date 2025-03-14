c83870346383e4ef81fa3ad8271cfee14743a254
Iznmav <andreijhosephgania@gmail.com>
2025-03-14 15:47:21 -0400

    q2 fix

--- Week10/main.py
+++ Week10/main.py
@@ -14,10 +14,10 @@ try:
             except Exception as e:
                 print(f"Error executing Query 1")
             try:
-                del_row = int(input("Enter the row ID threshold for deletion"))
+                del_row = int(input("Enter the row ID threshold for deletion: "))
                 query_2 = "DELETE FROM demo WHERE ID < ?"
                 cursor.execute(query_2, (del_row, ))
-                num_rows = cursor.row_count
+                num_rows = cursor.rowcount
                 print(f"{num_rows} rows affected!")
                 db_conn.commit()
             except Exception as e:
